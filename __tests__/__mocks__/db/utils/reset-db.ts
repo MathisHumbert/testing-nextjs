112f5c906e3269891d5f90449dc669cf9242828e
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-07-29 10:18:22 +0200

    setting up a test database done

---
+++ __tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,25 @@
+import { filenames, writeJSONToFile } from '@/lib/db/db-utils';
+
+import { readFakeData } from '../../fakeData';
+
+export const resetDB = async () => {
+  // failsafe against resetting production db!
+  const safeToReset = process.env.NODE_ENV === 'test';
+
+  if (!safeToReset) {
+    // eslint-disable-next-line no-console
+    console.log('WARNING: database reset unvailable outside test environment');
+    return;
+  }
+
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } =
+    await readFakeData();
+
+  // overwrite data in files
+  await Promise.all([
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.users, fakeUsers),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+  ]);
+};
